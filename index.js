9a1c17d539229d26a9937143a4da3121f4291a60
Muhammad Hamza Farrukh <mfarrukh@purdue.edu>
2019-06-01 15:59:51 -0400

    A lot of refactoring

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const fs = require('fs');
-const Fortnite = require("fortnite-api");
+const Fortnite = require('fortnite-api');
 const Discord = require('discord.js');
-
-const client = new Discord.Client();
+const utils = require('./utils');
 
 if (process.argv.length === 2) {
     const result = require('dotenv').config();
 
     if (result.error) {
         console.log(result.error);
-        writeToFile(result.error);
+        utils.writeToFile(result.error);
         process.exit(1);
     }
 }
 
 let logged_in_discord = false;
 let channel;
+
+const client = new Discord.Client();
+discordLogin(client);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
@@ -27,28 +28,33 @@ client.on('message', msg => {
     }
 });
 
-client.login(process.env.bot_token).then(message => {
-    if (message !== process.env.bot_token) {
-        const error = 'Wrong token returned after logging in';
+
+function discordLogin(client) {
+    client.login(process.env.bot_token).then(message => {
+        if (message !== process.env.bot_token) {
+            const error = 'Wrong token returned after logging in';
+            console.log(error);
+            utils.writeToFile(error);
+            process.exit(1);
+        } else {
+            logged_in_discord = true;
+            // noinspection JSUnresolvedVariable
+            channel = client.channels.find(value => value.id === process.env.channel_id);
+        }
+    }, error => {
         console.log(error);
-        writeToFile(error);
-    } else {
-        logged_in_discord = true;
-        // noinspection JSUnresolvedVariable
-        channel = client.channels.find(value => value.id === process.env.channel_id);
-    }
-}, err => {
-    console.log(err);
-});
+        utils.writeToFile(error);
+        process.exit(1);
+    });
+}
+
 
 const fortnite_credentials = [
     process.env.email,
     process.env.password,
     process.env.fortnite_launcher_token,
-    process.env.fortnite_client_token];
-
-let fortniteAPI = new Fortnite(fortnite_credentials);
-
+    process.env.fortnite_client_token
+];
 
 const players = [
     ['Hamza141', 'pc'],
@@ -59,49 +65,40 @@ const players = [
     // ['Tfue', 'pc']
 ];
 
+let cache = utils.loadCache();
+utils.writeToFile(cache);
 
-let cache = loadCache();
-writeToFile(cache);
 let intervalID;
+let current_index = 0;
+const num_players = players.length;
 
+let fortniteAPI = new Fortnite(fortnite_credentials);
 fortniteLogin();
+
 
 function fortniteLogin() {
     fortniteAPI.login().then(() => {
         intervalID = setInterval(getMatchDate, 2500);
-    }).catch(err => {
-        console.log(err);
+    }).catch(error => {
+        console.log(error);
+        utils.writeToFile(error);
+        process.exit(1);
     });
 }
 
+
 function restartFortnite() {
     const output = 'Restarting fortnite api';
-    console.log([getTimestamp(), output]);
-    writeToFile(output);
+    console.log([utils.getTimestamp(), output]);
+    utils.writeToFile(output);
 
+    logged_in_discord = false;
     clearInterval(intervalID);
-    fortniteAPI.kill();
-    fortniteLogin();
+    fortniteAPI.kill().then(() => {
+        fortniteLogin();
+    });
 }
 
-
-function getTimestamp() {
-    return new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
-}
-
-function getInterval() {
-    const num = Object.keys(players).length;
-    if (num <= 2) {
-        return 4000;
-    }
-    if (num < 5) {
-        return num * 2000;
-    }
-    return 10000;
-}
-
-let current_index = 0;
-const num_players = players.length;
 
 function getMatchDate() {
     const player = players[current_index];
@@ -112,10 +109,10 @@ function getMatchDate() {
     fortniteAPI
         .getStatsBR(username, platform, "weekly")
         .then(stats => {
-            updateCache(username, stats.group);
+            processStats(username, stats.group);
         })
         .catch(err => {
-            console.log([getTimestamp(), err, username, platform]);
+            console.log([utils.getTimestamp(), err, username, platform]);
             restartFortnite();
         });
 
@@ -123,7 +120,7 @@ function getMatchDate() {
 }
 
 
-function updateCache(username, stats) {
+function processStats(username, stats) {
     if (username === undefined || stats === undefined) {
         return;
     }
@@ -136,12 +133,12 @@ function updateCache(username, stats) {
 
         if (stats_updated) {
             cache[username] = stats;
-            dumpCache();
-            writeToFile({username: stats});
+            utils.dumpCache(cache);
+            utils.writeToFile({username: stats});
         }
     } else {
         cache[username] = stats;
-        dumpCache();
+        utils.dumpCache(cache);
     }
 }
 
@@ -169,12 +166,12 @@ function sendMessage(username, win, kills) {
                 description += ' (in ' + kills[2] + ' matches - this shouldn\'t happen)'
             }
             embed.setTitle(title)
-                .setColor(StringToColor.next(username))
+                .setColor(utils.getColor.next(username))
                 .setDescription(description);
             channel.send(embed);
-            console.log(getTimestamp());
+            console.log(utils.getTimestamp());
             console.log(description);
-            writeToFile(description);
+            utils.writeToFile(description);
         }
 
         if (win != null || kills != null) {
@@ -210,75 +207,3 @@ function compareKills(username, stats) {
     // todo don't return null
     return null;
 }
-
-
-function writeToFile(object) {
-    fs.appendFileSync("output.txt", getTimestamp() + " " + JSON.stringify(object, null, 2) + "\n\n\n",
-        function (err) {
-            if (err) {
-                console.log(object);
-                return console.log(err);
-            }
-        });
-}
-
-function dumpCache() {
-    fs.writeFileSync('cache.json', JSON.stringify(cache, null, 2), 'utf-8');
-}
-
-
-function loadCache() {
-    if (fs.existsSync('cache.json')) {
-        let old_cache;
-        try {
-            old_cache = JSON.parse(fs.readFileSync('cache.json', 'utf-8'));
-        } catch (error) {
-            console.log(getTimestamp());
-            console.log(error);
-            writeToFile(error);
-            return {};
-        }
-        return old_cache;
-    } else {
-        return {};
-    }
-}
-
-
-// Takes any string and converts it into a #RRGGBB color.
-let StringToColor = (function () {
-    let instance = null;
-
-    return {
-        next: function stringToColor(str) {
-            if (instance === null) {
-                instance = {};
-                instance.stringToColorHash = {};
-                instance.nextVeryDifferntColorIdx = 0;
-                instance.veryDifferentColors = [
-                    "#7544B1", "#B500FF", "#00FF78", "#FF6E41",
-                    "#FF0000", "#00FF00", "#0000FF", "#000000",
-                    "#01FFFE", "#FFA6FE", "#FFDB66", "#006401",
-                    "#010067", "#95003A", "#007DB5", "#FF00F6",
-                    "#FFEEE8", "#774D00", "#90FB92", "#0076FF",
-                    "#D5FF00", "#FF937E", "#6A826C", "#FF029D",
-                    "#FE8900", "#7A4782", "#7E2DD2", "#85A900",
-                    "#FF0056", "#A42400", "#00AE7E", "#683D3B",
-                    "#BDC6FF", "#263400", "#BDD393", "#00B917",
-                    "#9E008E", "#001544", "#C28C9F", "#FF74A3",
-                    "#01D0FF", "#004754", "#E56FFE", "#788231",
-                    "#0E4CA1", "#91D0CB", "#BE9970", "#968AE8",
-                    "#BB8800", "#43002C", "#DEFF74", "#00FFC6",
-                    "#FFE502", "#620E00", "#008F9C", "#98FF52",
-                    "#005F39", "#6B6882", "#5FAD4E", "#A75740",
-                    "#A5FFD2", "#FFB167", "#009BFF", "#E85EBE"
-                ];
-            }
-
-            if (!instance.stringToColorHash[str])
-                instance.stringToColorHash[str] = instance.veryDifferentColors[instance.nextVeryDifferntColorIdx++];
-
-            return instance.stringToColorHash[str];
-        }
-    }
-})();
---
+++ utils.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+
+
+function writeToFile(object) {
+    fs.appendFileSync("output.txt", getTimestamp() + " " + JSON.stringify(object, null, 2) + "\n\n\n",
+        function (err) {
+            if (err) {
+                console.log(object);
+                return console.log(err);
+            }
+        });
+}
+
+
+function dumpCache(cache) {
+    fs.writeFileSync('cache.json', JSON.stringify(cache, null, 2), 'utf-8');
+}
+
+
+function loadCache() {
+    if (fs.existsSync('cache.json')) {
+        let old_cache;
+        try {
+            old_cache = JSON.parse(fs.readFileSync('cache.json', 'utf-8'));
+        } catch (error) {
+            console.log(getTimestamp());
+            console.log(error);
+            writeToFile(error);
+            return {};
+        }
+        return old_cache;
+    } else {
+        return {};
+    }
+}
+
+
+// Takes any string and converts it into a #RRGGBB color.
+let getColor = (function () {
+    let instance = null;
+
+    return {
+        next: function stringToColor(str) {
+            if (instance === null) {
+                instance = {};
+                instance.stringToColorHash = {};
+                instance.nextVeryDifferntColorIdx = 0;
+                instance.veryDifferentColors = [
+                    "#7544B1", "#B500FF", "#00FF78", "#FF6E41",
+                    "#FF0000", "#00FF00", "#0000FF", "#000000",
+                    "#01FFFE", "#FFA6FE", "#FFDB66", "#006401",
+                    "#010067", "#95003A", "#007DB5", "#FF00F6",
+                    "#FFEEE8", "#774D00", "#90FB92", "#0076FF",
+                    "#D5FF00", "#FF937E", "#6A826C", "#FF029D",
+                    "#FE8900", "#7A4782", "#7E2DD2", "#85A900",
+                    "#FF0056", "#A42400", "#00AE7E", "#683D3B",
+                    "#BDC6FF", "#263400", "#BDD393", "#00B917",
+                    "#9E008E", "#001544", "#C28C9F", "#FF74A3",
+                    "#01D0FF", "#004754", "#E56FFE", "#788231",
+                    "#0E4CA1", "#91D0CB", "#BE9970", "#968AE8",
+                    "#BB8800", "#43002C", "#DEFF74", "#00FFC6",
+                    "#FFE502", "#620E00", "#008F9C", "#98FF52",
+                    "#005F39", "#6B6882", "#5FAD4E", "#A75740",
+                    "#A5FFD2", "#FFB167", "#009BFF", "#E85EBE"
+                ];
+            }
+
+            if (!instance.stringToColorHash[str])
+                instance.stringToColorHash[str] = instance.veryDifferentColors[instance.nextVeryDifferntColorIdx++];
+
+            return instance.stringToColorHash[str];
+        }
+    }
+})();
+
+
+function getTimestamp() {
+    return new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+}
+
+
+function getInterval() {
+    const num = Object.keys(players).length;
+    if (num <= 2) {
+        return 4000;
+    }
+    if (num < 5) {
+        return num * 2000;
+    }
+    return 10000;
+}
+
+
+module.exports = {
+    writeToFile: writeToFile,
+    loadCache: loadCache,
+    dumpCache: dumpCache,
+    getColor: getColor,
+    getTimestamp: getTimestamp,
+    getInterval: getInterval
+};
